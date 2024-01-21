16a365821ba7ac2b456f1a576c1d7ff60f6c86d9
amedduman <amedduman@protonmail.com>
2024-01-21 18:42:18 +0300

    add iterators to stack data type

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "Stack.h"
 
 int main()
 {
+    using namespace std;
 
+    Stack s;
+    s.Push( 1 );
+    s.Push( 2 );
+    s.Push( 3 );
+    s.Push( 4 );
+
+    s.Print();
+
+    // multiply all elements by 10
+    for( auto& n : s )
+    {
+        *n *= 10;
+    }
+
+    cout << "after mul " << endl;
+
+    s.Print();
 }
